56d0cd5d1d460336d1745bfaf2f8371a2606a47b
dankags <danielkagombe70@gmail.com>
2024-03-27 17:25:40 +0300

    modified:   components/searchCard/SearchCard.jsx

--- components/searchCard/SearchCard.jsx
+++ components/searchCard/SearchCard.jsx
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 import React, { useEffect } from 'react'
 import "./SearchCard.globals.css"
-import { useRouter } from 'next/navigation'
+
 
 const SearchCard = ({ category, pageCurrentWidth }) => {
  
 
   return (
-    <div className={` w-full aspect-video relative rounded-lg  overflow-hidden p-3 text-white cursor-pointer   `} style={category?{backgroundColor:`${category.backgroundColor}`}:{backgroundColor:"rgb(255,255,255,0.6)"}}>
+    <div className={` w-full h-24 md:aspect-video relative rounded-md lg:rounded-lg overflow-hidden  p-3 text-white cursor-pointer   `} style={category?{backgroundColor:`${category.backgroundColor}`}:{backgroundColor:"rgb(255,255,255,0.6)"}}>
       <span className='font-bold max-sm:text-base max-md:text-lg md:text-xl'>{ category.category}</span>   
-      <div className={`  ${pageCurrentWidth <= 700 ? " -bottom-0 -right-3" : " -bottom-20 -right-28"} absolute  `}>
-             <div className='relative min-h-20 min-w-20'>
+      <div className={`  bottom-0 -right-4  absolute  `}>
+             <div className='relative w-14 h-14 md:w-16 md:h-16 xl:h-20 xl:w-20'>
               <Image src={category. backgroundImg} alt="" fill={true} className='imageRotation'/>
               </div>
       </div>
