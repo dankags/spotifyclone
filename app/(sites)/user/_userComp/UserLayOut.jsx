9a05156567b23b38ef3482592bde4ab113cf8ada
dankags <danielkagombe70@gmail.com>
2024-03-29 14:12:16 +0300

    modified:   app/(sites)/user/_userComp/UserLayOut.jsx

--- app/(sites)/user/_userComp/UserLayOut.jsx
+++ app/(sites)/user/_userComp/UserLayOut.jsx
@@ -32,96 +32,118 @@ const UserLayOut = ({children,followings,followers,playlist,paramsId,user,bgColo
   };
   
   return (
-    <div className='w-full h-full rounded-md  top-0' style={{backgroundColor:`${imgNameChoosen ? choosenImgColor : "rgba(127.5,127.5,127.5,1)"}`}}>
-        <ScrollArea onScrollCapture={handleScroll} className='w-full h-full rounded-md bg-neutral-900/30'>
-          <main className='relative'>
-        <div className='sticky z-10 top-0'>
-          <NavBar bgColor={navColor ? `${navBgColor}`:""}/>
-        </div>
-        <section className={` w-full flex items-end gap-3 pl-4 pb-4 relative `}>
-             
-            <div
-              className={`pb-1 flex justify-start w-3/12    `}
+    <div
+      className="w-full h-full rounded-md  top-0"
+      style={{
+        backgroundColor: `${
+          imgNameChoosen ? choosenImgColor : "rgba(127.5,127.5,127.5,1)"
+        }`,
+      }}
+    >
+      <ScrollArea
+        onScrollCapture={handleScroll}
+        className="w-full h-full rounded-md bg-neutral-900/30"
+      >
+        <main className="relative">
+          <div className="sticky z-10 top-0">
+            <NavBar bgColor={navColor ? `${navBgColor}` : ""} />
+          </div>
+          <section
+            className={` w-full flex items-end gap-3 pl-4 pb-4 relative `}
+          >
+            <div className={`pb-1 flex justify-start w-3/12    `}>
+              {paramsId === data?.user.id ? (
+                <UserDialog setImgName={setImgNameChoosen}>
+                  <button
+                    className={`group relative w-full aspect-square cursor-pointer `}
                   >
-             {paramsId === data?.user.id ?
-              <UserDialog setImgName={setImgNameChoosen} >
-              <button  className={`group relative w-full aspect-square cursor-pointer `}>       
-            {data?.user.image ?  <Image
-                src={data?.user.image?data?.user.image:imgNameChoosen.img }
-                alt="userImage"
-                fill
-                className="object-cover shadow-2xl shrink-0 shadow-black rounded-full"
-                    />
-                      :
-                      <LuUser2  className='m-auto text-7xl text-stone-300'/> 
-                   }
-                <div className='w-full h-full absolute top-0 left-0 rounded-full flex flex-col justify-center items-center gap-2 opacity-0 transition duration-300 text-neutral-50 group-hover:bg-neutral-900/75 group-hover:opacity-100'>
-                  <MdOutlineEdit className='text-6xl'/> 
-                   <span className='text-lg font-semibold '>Choose photo</span>            
-                </div>                
-              </button> 
-                </UserDialog>  
-                :
-                <div  className={`group relative w-full aspect-square `}>       
-                <Image
-                  src={data?.user.image?data?.user.image:"" }
-                  alt="likedImage"
-                  fill
-                  className="object-cover shadow-2xl shrink-0 shadow-black rounded-full"
-                                  />              
-                </div> 
-              }   
+                    {data?.user.image ? (
+                      <Image
+                        src={
+                          data?.user.image
+                            ? data?.user.image
+                            : imgNameChoosen.img
+                        }
+                        alt="userImage"
+                        fill
+                        className="object-cover  shrink-0 shadow-[0_4px_60px_0] shadow-black/50 rounded-full"
+                      />
+                    ) : (
+                      <LuUser2 className="m-auto text-7xl text-stone-300" />
+                    )}
+                    <div className="w-full h-full absolute top-0 left-0 rounded-full flex flex-col justify-center items-center gap-2 opacity-0 transition duration-300 text-neutral-50 group-hover:bg-neutral-900/75 group-hover:opacity-100">
+                      <MdOutlineEdit className="text-6xl" />
+                      <span className="text-lg font-semibold ">
+                        Choose photo
+                      </span>
+                    </div>
+                  </button>
+                </UserDialog>
+              ) : (
+                <div className={`group relative w-full aspect-square `}>
+                  <Image
+                    src={data?.user.image ? data?.user.image : ""}
+                    alt="likedImage"
+                    fill
+                    className="object-cover shadow-2xl shrink-0 shadow-black rounded-full"
+                  />
+                </div>
+              )}
             </div>
             <div
               className={`pb-1 mt-4 h-full w-9/12 pl-2 flex flex-col justify-end`}
             >
-              <section className='flex flex-col justify-end'>
+              <section className="flex flex-col justify-end">
                 <div className={``}>
-                  <span className="font-medium text-neutral-50 drop-shadow-xl textShadow">Profile</span>
+                  <span className="font-medium text-neutral-50 drop-shadow-xl textShadow">
+                    Profile
+                  </span>
                 </div>
                 <div>
-           { paramsId === data?.user.id ?  
-              <UserDialog setImgName={setImgNameChoosen}>
-                  <button
-                    className={`w-[90%] cursor-pointer text-left text-8xl py-3 font-extrabold text-neutral-50  truncate capitalize `}
-                  >
-                  {data?.user.name?data?.user.name:imgNameChoosen.name }
-                  </button>
-                  </UserDialog>
-                  :
-                  <span
-                    className={`w-[90%]  text-left text-8xl py-2 font-extrabold text-neutral-50  truncate capitalize textShadow`}
-                  >
-                  {data?.user.name?data?.user.name:imgNameChoosen.name }
-                  </span>
-
-                }
+                  {paramsId === data?.user.id ? (
+                    <UserDialog setImgName={setImgNameChoosen}>
+                      <button
+                        className={`w-[90%] cursor-pointer text-left text-8xl py-3 font-extrabold text-neutral-50  truncate capitalize `}
+                      >
+                        {data?.user.name
+                          ? data?.user.name
+                          : imgNameChoosen.name}
+                      </button>
+                    </UserDialog>
+                  ) : (
+                    <span
+                      className={`w-[90%]  text-left text-8xl py-2 font-extrabold text-neutral-50  truncate capitalize textShadow`}
+                    >
+                      {data?.user.name ? data?.user.name : imgNameChoosen.name}
+                    </span>
+                  )}
                 </div>
                 <div className={`flex items-center gap-x-2 pt-7`}>
                   <span className="mr-2 text-sm text-neutral-50 font-semibold drop-shadow-xl textShadow">
-                    { playlist.length } Public Playlist  .
+                    {playlist.length} Public Playlist .
                   </span>
                   <span className="text-sm text-left text-neutral-50 font-medium  drop-shadow-xl textShadow ">
-                  { followings.length } Following
+                    {followings.length} Following
                   </span>
-                  {followers.length > 0  ? <span className="text-sm text-neutral-50 font-medium drop-shadow-xl textShadow">
-                    {followers.length} Followers
-                  </span>
-                  :
-                  <></>
-                  }
+                  {followers.length > 0 ? (
+                    <span className="text-sm text-neutral-50 font-medium drop-shadow-xl textShadow">
+                      {followers.length} Followers
+                    </span>
+                  ) : (
+                    <></>
+                  )}
                 </div>
               </section>
             </div>
-              </section>
-        <div className='bg-gradient-to-t from-90% from-neutral-900 to-neutral-900/20'>
-        {children}
-        </div>
-        <Footer/>
+          </section>
+          <div className="bg-gradient-to-t from-90% from-neutral-900 to-neutral-900/20">
+            {children}
+          </div>
+          <Footer />
         </main>
-        </ScrollArea>
+      </ScrollArea>
     </div>
-  )
+  );
 }
 
 export default UserLayOut
